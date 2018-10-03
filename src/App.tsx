dad2f64c8b102c7e47e0d4ea2eca0f9e52b48204
Steve Spicklemire <steve@spvi.com>
2018-10-03 08:57:57 -0400

    simple example

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import * as React from 'react';
 import './App.css';
 
-class App extends React.Component {
-  public render() {
-    return (
-        <table>
+class ButtonControl extends React.Component<{onButton:() => void}> {
+    public render() {
+        return (<input type="button" onClick={this.props.onButton} value="Click me"/>)
+    }
+}
+
+class ColorTable extends React.Component<{doGreen:boolean}> {
+    public render() {
+        return (
+        <table className={this.props.doGreen? 'App-green' : 'App-red'}>
             <thead>
                 <tr><th>Number</th><th>Name</th></tr>
             </thead>
@@ -16,8 +22,39 @@ class App extends React.Component {
                 <tr><td>4</td><td>Julie</td></tr>
             </tbody>
         </table>
-    );
-  }
+        )
+    }
+}
+
+class Combo extends React.Component<{},{buttonState:boolean}> {
+
+    constructor(props:any) {
+        super(props)
+        this.state = {buttonState:false}
+        this.handleButtonClick = this.handleButtonClick.bind(this)
+    }
+    
+    handleButtonClick() {
+        this.setState({buttonState:!this.state.buttonState})
+    }
+    
+    public render() {
+        return (
+            <div>
+            <ButtonControl onButton={this.handleButtonClick}/>
+            <ColorTable doGreen={this.state.buttonState}/>
+            </div>
+        );
+    }
+
+}
+
+class App extends React.Component {
+    public render() {
+        return (
+            <Combo/>
+        )
+    }
 }
 
 export default App;
